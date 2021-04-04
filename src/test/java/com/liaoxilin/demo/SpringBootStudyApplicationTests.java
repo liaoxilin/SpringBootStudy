ebf5deb7f6808436a809f20106d294687fdfe6f6
liaoxilin <1193273390@qq.com>
2021-04-04 10:54:59 +0800

    Initial commit

---
+++ src/test/java/com/liaoxilin/demo/SpringBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liaoxilin.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
